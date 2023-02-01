67d674481843689357a4b41e60b50c818c7c318c
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-02-01 17:00:29 -0500

    create app.ts

---
+++ backend/src/app.ts
@@ -0,0 +1,17 @@
+import { authRouter } from './auth/auth.routes';
+import express from 'express';
+import morgan from 'morgan';
+const app = express();
+
+// Middlewares
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes
+app.get('/', (_req, res) => {
+  return res.json({ message: 'Hello world' });
+});
+app.use(authRouter);
+
+export default app;
