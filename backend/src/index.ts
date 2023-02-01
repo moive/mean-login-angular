980050c56136d9acce7eb142adddb68a8939dc30
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-01-31 23:46:03 -0500

    add const PORT

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,15 @@
 import { logger } from './config';
+import 'dotenv/config';
+
 import express from 'express';
 
 const app = express();
+const PORT = process.env['PORT'] ?? '4000';
 
 app.get('/', (_req, res) => {
   return res.json({ message: 'Hello world' });
 });
 
-app.listen(4000, () => {
-  logger.info('Server started on port 4000');
+app.listen(PORT, () => {
+  logger.info(`Server started on port ${PORT}`);
 });
