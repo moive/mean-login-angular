120c865b8cee36bca8d601b9d2f44c6d036b167b
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-02-02 16:12:20 -0500

    implementation model interfaces

--- frontend/src/app/models/jwt-response.ts
+++ frontend/src/app/models/jwt-response.ts
@@ -1,2 +1,7 @@
-export interface JwtResponseTs {
+export interface IJwtResponseTs {
+  id: number;
+  name: string;
+  email: string;
+  token: string;
+  expiresIn: number;
 }
--- frontend/src/app/models/user.ts
+++ frontend/src/app/models/user.ts
@@ -1,2 +1,6 @@
-export interface User {
+export interface IUser {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
 }
