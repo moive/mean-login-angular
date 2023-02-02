1888983d32433b05fed45db79fb266c594da4e07
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-02-02 10:55:28 -0500

    conexion db

---
+++ backend/src/database.ts
@@ -0,0 +1,8 @@
+import { connect } from 'mongoose';
+
+async function dbConnect(): Promise<void> {
+  const DB_URI = process.env['DB_URI'] as string;
+  await connect(DB_URI);
+}
+
+export default dbConnect();
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,15 @@
 import app from './app';
 import { logger } from './config';
 import 'dotenv/config';
+import dbConnect from './database';
+
+dbConnect
+  .then(() => {
+    logger.info('Conexion Ready');
+  })
+  .catch((e) => {
+    logger.error(e);
+  });
 
 const PORT = process.env['PORT'] ?? '4000';
 
