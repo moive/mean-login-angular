4ff6826cb85e8a2c15dfce63361003b73adcd8c3
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-02-02 22:12:53 -0500

    fixes

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -3,18 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { HttpClientModule } from '@angular/common/http';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [RegisterComponent, LoginComponent],
-  imports: [
-    CommonModule,
-    AuthRoutingModule,
-    FormsModule,
-    AuthRoutingModule,
-    HttpClientModule,
-  ],
+  imports: [CommonModule, AuthRoutingModule, FormsModule, AuthRoutingModule],
 })
 export class AuthModule {}
