602e57e07c206bb74b206b6ea71738f64a9a9a29
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-02-02 19:02:43 -0500

    logic services

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,9 +1,57 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { IJwtResponse } from '../models/jwt-response';
+import { IUser } from '../models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  auth_server: string = 'http://localhost:4001';
+  authSubject = new BehaviorSubject(false);
+  private token: string = '';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  register(user: IUser): Observable<IJwtResponse> {
+    return this.httpClient
+      .post<IJwtResponse>(`${this.auth_server}/register`, user)
+      .pipe(
+        tap((res: IJwtResponse) => {
+          if (res) {
+            this.saveToken(res.dataUser.token, res.dataUser.expiresIn);
+          }
+        })
+      );
+  }
+
+  login(user: IUser): Observable<IJwtResponse> {
+    return this.httpClient
+      .post<IJwtResponse>(`${this.auth_server}/login`, user)
+      .pipe(
+        tap((res: IJwtResponse) => {
+          if (res) {
+            this.saveToken(res.dataUser.token, res.dataUser.expiresIn);
+          }
+        })
+      );
+  }
+
+  logout(): void {
+    this.token = '';
+    localStorage.removeItem('ACCESS_TOKEN');
+    localStorage.removeItem('EXPIRES_IN');
+  }
+
+  private saveToken(token: string, expiresIn: string): void {
+    localStorage.setItem('ACCESS_TOKEN', token);
+    localStorage.setItem('EXPIRES_IN', expiresIn);
+    this.token = token;
+  }
+
+  private getToken(): string {
+    if (!this.token) localStorage.getItem('ACCESS_TOKEN');
+    return this.token;
+  }
 }
