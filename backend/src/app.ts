3a1dfe927f1fde6605813f5a8def7aced487a1e9
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-02-02 23:17:17 -0500

    fixes cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,14 @@
 import { authRouter } from './auth/auth.routes';
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
+
 const app = express();
 
 // Middlewares
-
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.get('/', (_req, res) => {
