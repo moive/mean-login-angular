23703c342e039644c371d8ab337fc29b18b7a9d4
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-02-02 23:35:17 -0500

    logic register

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
+  onRegister(form: NgForm): void {
+    this.authService.register(form.value).subscribe((res) => {
+      this.router.navigateByUrl('/auth');
+    });
+  }
 }
